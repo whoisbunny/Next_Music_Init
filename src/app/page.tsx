05111f72653560e236ebd50657e09778c99b055e
Bunny <kamleshrathod2092000@gmail.com>
2024-04-09 02:24:33 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased ">
+      <h1 className="text-center">Hello</h1>
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
+  );
+}
